65eb34014e18c2964b512c8d436d65c86d7cbf08
Fernando Torija <fer.torija+1@gmail.com>
2021-07-06 16:10:13 -0300

    commit

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Move this script into your .bashrc and change the path were you clone the .dotfile project.
+# Move this script into your .bashrc and change the path were you clone the .dotfile project
 for DOTFILE in `find ~/projects/dotfiles/system`
 do
   [ -f "$DOTFILE" ] && source "$DOTFILE"
